200bd7997355f8b0d1f27d59462f9918cdab3f96
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-22 23:11:51 -0400

    Added redux store and updated App tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
